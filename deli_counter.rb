c282a2ddac4d69d5b7885dc08727db2f6ec6ee5c
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-24 20:40:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,14 @@
 katz_deli = []
 
 def line(katz_deli)
-  line_postion = []
+  line_position = []
 
   if katz_deli.empty?
     puts "The line is currently empty."
   else
     number_count = 1
     katz_deli.each do |customer|
-      line_postion << "#{number_count}. #{customer}"
+      line_position << "#{number_count}. #{customer}"
       number_count += 1
     end
     puts "The line is currently: #{line_position.join(" ")}"
