21d590a73c432138f7a84155a04b752770f91126
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-24 20:33:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 katz_deli = []
-
+=begin 
 def line(katz_deli)
   in_line_postion = []
 
@@ -12,6 +12,21 @@ def line(katz_deli)
       in_line_postion += 1
     end
 
-    puts in_line_position#puts "The line is currently: #{in_line_position.join(" ")}"
+    puts in_line_position
+    #puts "The line is currently: #{in_line_position.join(" ")}"
+  end
+end
+=end
+def line(katz_deli)
+  line_positions = [];
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    number = 1
+    katz_deli.each do |customer|
+      line_positions << "#{number}. #{customer}"
+      number += 1
+    end
+    puts "The line is currently: #{line_positions.join(" ")}"
   end
 end
