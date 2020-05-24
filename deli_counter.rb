4a90dde99516dfac0e0d0e32e5d1610c46e193d5
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-24 20:29:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
   else
     number_count = 1
     katz_deli.each do |customer|
-      in_line_postion << "#{number_count}. #{customer}."
+      in_line_postion << "#{number_count}. #{customer}"
       in_line_postion += 1
     end
     puts "The line is currently: #{in_line_position.join(" ")}"
